d0b03f1991fc156303500d8fdd7a64385f7f6b3b
yang.chen <yang.chen@gllue.com>
2020-05-27 21:00:40 +0800

    :star: 产生式生成 以及 消除左子树

--- src/parser/ast/Expr.js
+++ src/parser/ast/Expr.js
@@ -1,14 +1,17 @@
 const ASTNode = require("./ASTNode")
 const ASTNodeTypes = require("./ASTNodeTypes")
+const table = require("../util/PriorityTable")
+const Factor = require("../ast/Factor")
+
 
 class Expr extends ASTNode {
 
-    constructor(parent) {
-        super(parent)
+    constructor() {
+        super()
     }
 
-    formToken(parent, type, token) {
-        const expr = new Expr(parent);
+    static formToken(type, token) {
+        const expr = new Expr();
         expr.label = token.getValue();
         expr.lexeme = token;
         expr.type = type;
@@ -23,19 +26,91 @@ class Expr extends ASTNode {
      * @param {*} parent 
      * @param {*} it 
      */
-    static parseExpr(parent, it) {
+    static parseExpr(it) {
+        return Expr.E(it, 0);
+    }
+
+    static E(it, k) {
+        if (k < table.length - 1) {
+            return Expr.combine(
+                it,
+                () => Expr.E(it, k + 1),
+                () => Expr.E_(it, k)
+            )
+        } else {
+            return Expr.race(
+                it,
+                () =>
+                    Expr.combine(
+                        it,
+                        () => Expr.F(it),
+                        () => Expr.E_(it, k)),
+                () => Expr.combine(
+                    it,
+                    () => Expr.U(it),
+                    () => Expr.E_(it, k)
+                )
+            )
+        }
 
     }
 
-    static E() {
+    static E_(it, k) {
+        const token = it.peek();
+        const value = token.getValue();
+
+        if (table[k].indexOf(value) !== -1) {
+            it.nextMatch(value);
+            const expr = Expr.fromToken(ASTNodeTypes.BINARY_EXPR, token);
+            expr.addChild(
+                Expr.combine(
+                    it,
+                    () => Expr.E(it, k + 1),
+                    () => Expr.E_(it, k, it)
+                )
+            );
+
+            return expr;
+        }
+        return null;
+    }
+
+    // Factor
+    static F(it) {
+        const factor = Factor.parse(it);
+        if (factor == null) {
+            return null;
+        }
+        if (it.hasNext() && it.peek().getValue() === "(") {
+            return CallExpr.parse(factor, it);
+        }
+        return factor;
+    }
+
+    // Unary 一元表达式
+    static U(it) {
+        const token = it.peek()
+        const value = token.getValue();
+
+        if (value === "(") {
+            it.nextMatch("(");
+            const expr = Expr.parseExpr(it);
+            it.nextMatch(")");
+            return expr;
+        } else if (value === "++" || value === "--" || value === "!") {
+            const t = it.peek();
+            t.nextMatch(value);
+
+            const expr = Expr.formToken(ASTNodeTypes.UNARY_EXPR, t);
+            expr.addChild(Expr.parseExpr(it));
+            return expr;
+        }
+
+        return null;
 
     }
 
-    static E_() {
-
-    }
-
-    static combine(parent, it, funcA, funcB) {
+    static combine(it, funcA, funcB) {
         if (!it.hasNext()) {
             return null
         }
@@ -47,7 +122,7 @@ class Expr extends ASTNode {
         const b = it.hasNext() ? funcB() : null;
         if (b == null) { return a }
 
-        const expr = Expr.formToken(parent, ASTNodeTypes.BINARY_EXPR, b.lexeme)
+        const expr = Expr.formToken(ASTNodeTypes.BINARY_EXPR, b.lexeme)
         expr.addChild(a)
         expr.addChild(b.getChild(0))
         return expr;
--- src/parser/util/PriorityTable.js
+++ src/parser/util/PriorityTable.js
@@ -1,26 +1,7 @@
-// 处理表达式的优先级
-class PriorityTable {
-
-    constructor() {
-        this.table = [
-            ["&", "|", "^."],
-            ["==", "!=", ">", "<", ">="],
-            ["+", "-"],
-            ["*", "/"],
-            ["<<", ">>"]
-        ];
-    }
-
-    getSize() {
-        return this.table.length;
-    }
-
-    getLevelList(level) {
-        if(!this.table[level]){
-            throw new Error("error Level")
-        }
-
-        return this.table[level];
-    }
-
-} 
+module.exports = [
+    ["&", "|", "^."],
+    ["==", "!=", ">", "<", ">="],
+    ["+", "-"],
+    ["*", "/"],
+    ["<<", ">>"]
+];
